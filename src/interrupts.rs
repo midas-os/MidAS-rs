8349a17da483c51577d5b8d7a1f454c30306aef0
Mindless Sea <averytoad65@gmail.com>
2023-01-29 21:16:57 +0100

    add stack_overflow handling

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,11 +1,24 @@
+/**************************************************************************************************
+* Name : 									 interrupts.rs
+* Author : 										Avery
+* Date : 									  1/29/2023
+* Purpose : 				   InterruptDescriptorTable and Exceptions
+* Version : 									 0.1
+**************************************************************************************************/
+
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
-use crate::{print,println,change_fg,vga_buffer,vga_buffer::Color};
+use crate::{gdt, println, change_fg, vga_buffer::Color};
 
 lazy_static! {
 static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        
         idt
     };
 }
@@ -22,4 +35,17 @@ extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: InterruptStackFrame)
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+/****************************************
+ * invoke a breakpoint exception
+****************************************/
+    x86_64::instructions::interrupts::int3();
 }
