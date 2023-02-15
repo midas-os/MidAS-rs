debda6739b8f70d271eaf84624920f1b5f6253db
Mindless Sea <averytoad65@gmail.com>
2023-02-15 18:15:11 +0100

    add midas_vga crate

--- src/vga_driver.rs
+++ src/vga_driver.rs
@@ -8,6 +8,7 @@
 
 
 use alloc::{string::ToString, format, vec::Vec};
+use midas_vga::graphics::{calculate_centered_rect, draw_rect, write_str_centered_x, draw_centered_rect};
 use pc_keyboard::{DecodedKey, KeyCode};
 use spin::Mutex;
 use vga::{colors::{Color16}, writers::{Graphics640x480x16, GraphicsWriter, Text80x25, TextWriter}, drawing::Point};
@@ -130,82 +131,6 @@ pub fn load_current_page() {
 
     let page = PAGES.lock()[unsafe { CURRENT_PAGE }].function;
     page();
-}
-
-pub fn draw_rect(position: Point<isize>, size: Point<isize>, color: Color16) {
-    let mode = Graphics640x480x16::new();
-    mode.set_mode();
-
-    mode.draw_line((position.0, position.1), (position.0, position.1 + size.1), color);
-    mode.draw_line((position.0, position.1), (position.0 + size.0, position.1), color);
-    mode.draw_line((position.0 + size.0, position.1), (position.0 + size.0, position.1 + size.1), color);
-    mode.draw_line((position.0, position.1 + size.1), (position.0 + size.0, position.1 + size.1), color);
-}
-
-pub fn write_string(start: Point<usize>, string: &str, color: Color16) {
-    let mode = Graphics640x480x16::new();
-    let mut y_offset = 0;
-    let mut x_offset = 0;
-
-    for (offset, character) in string.chars().enumerate() {
-        if character == '\n' {
-            x_offset = 0;
-            y_offset += 1;
-            continue;
-        }
-
-        x_offset += 1;
-        mode.draw_character(start.0 + x_offset * 8, start.1 + (y_offset * 10), character, color);
-    }
-}
-
-pub fn draw_centered_rect(size: Point<isize>, color: Color16) {
-    let x = (640 - size.0) / 2;
-    let y = (480 - size.1) / 2;
-
-    draw_rect((x, y), size, color);
-}
-
-pub fn calculate_centered_rect(size: Point<isize>) -> Point<isize> {
-    let x = (640 - size.0) / 2;
-    let y = (480 - size.1) / 2;
-
-    (x, y)
-}
-
-pub fn write_str_centered(bounds_start: Point<usize>, bounds_end: Point<usize>, string: &str, color: Color16) {
-    let lines = string.split('\n');
-
-    for (offset, line) in lines.enumerate() {
-        let string_width = line.len() * 8;
-        let string_height = 16;
-
-        let x = (bounds_start.0 + bounds_end.0) / 2 - string_width / 2;
-        let y = (bounds_start.1 + bounds_end.1) / 2 - string_height / 2;
-
-        write_string((x, y + (offset * 10)), line, color);
-    }
-}
-
-pub fn write_str_centered_x(bounds_start: Point<usize>, bounds_end: Point<usize>, y: usize, string: &str, color: Color16) {
-    // split string into new lines
-    let lines = string.split('\n');
-
-    for (offset, line) in lines.enumerate() {
-        let string_width = line.len() * 8;
-
-        let x = (bounds_start.0 + bounds_end.0) / 2 - string_width / 2;
-
-        write_string((x, y + (offset * 10)), line, color);
-    }
-}
-
-pub fn write_str_centered_y(bounds_start: Point<usize>, bounds_end: Point<usize>, x: usize, string: &str, color: Color16) {
-    let string_height = 16;
-
-    let y = (bounds_start.1 + bounds_end.1) / 2 - string_height / 2;
-
-    write_string((x, y), string, color);
 }
 
 pub fn register_key(key: DecodedKey) {
